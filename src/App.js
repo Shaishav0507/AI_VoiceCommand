0b7fd13586f3277d71fc0ec1b25a9c25cf48d652
Shivaansh17 <shivansh_rastogi@hotmail.com>
2022-02-02 23:04:43 +0530

    key added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = 'db5477f5a85cf27fb72cd6c8f3d726be2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const app = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key:alanKey,
+            onCommand: ({command}) => {
+                if(command === 'testCommand'){
+                    alert("This code was executed");
+                }
+            }
+        })
+    }, [])
+
+  return (
+  <div>
+      <h1>Alan AI News Applivations</h1>
+  </div>
+  );
+};
+
+export default app;
